c6c18ef8b7585a45aba547021d04526df6766277
kid1carus <kid1carus@mail.com>
2019-08-25 22:25:31 -0400

    added s4t and iskeu

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/white1ite debs
 dpkg-deb -bZgzip projects/unnamed debs
+dpkg-deb -bZgzip projects/iSkeuomorphic debs
+dpkg-deb -bZgzip projects/six4twelve debs

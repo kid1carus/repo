6c76866ab8b45a507d5e3b45906a918c8ad1a757
kid1carus <kid1carus@mail.com>
2019-08-29 23:19:47 -0400

    updates

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/white1ite debs
+dpkg-deb -bZgzip projects/White1ite debs
 dpkg-deb -bZgzip projects/unnamed debs
 dpkg-deb -bZgzip projects/iSkeuomorphic debs
 dpkg-deb -bZgzip projects/six4twelve debs

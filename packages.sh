43b5b087ad7c017395e0b1599a71b1e6d2691753
kid1carus <kid1carus@mail.com>
2019-09-01 15:37:31 -0400

    theme updates

--- packages.sh
+++ packages.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/White1ite debs
 dpkg-deb -bZgzip projects/unnamed debs
-dpkg-deb -bZgzip projects/iSkeuomorphic debs
-dpkg-deb -bZgzip projects/six4twelve debs

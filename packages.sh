921000b29a862ef7dd958f14577b581c1e88c00e
kid1carus <kid1carus@mail.com>
2020-10-18 13:23:01 -0400

    uploaded Embossed Dark theme to repo.

--- packages.sh
+++ packages.sh
@@ -6,5 +6,4 @@ dpkg-deb -bZgzip projects/RealWeather debs
 dpkg-deb -bZgzip projects/RealWeatherIcon debs
 dpkg-deb -bZgzip projects/iSkeuomorphic debs
 dpkg-deb -bZgzip projects/m1rroredblu13 debs
-dpkg-deb -bZgzip projects/deBOSSedxiii debs
-dpkg-deb -bZgzip projects/emBOSSedxiii debs
+dpkg-deb -bZgzip projects/Embossed Dark debs

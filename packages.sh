7a9eee06f216f36e554b884da9eefe4d54f8c433
kid1carus <kid1carus@mail.com>
2020-12-17 23:02:26 -0500

    added Red Neon theme to repo

--- packages.sh
+++ packages.sh
@@ -12,3 +12,5 @@ dpkg-deb -bZgzip projects/eTVDark debs
 dpkg-deb -bZgzip projects/iOSDarkWinter debs
 dpkg-deb -bZgzip projects/iOS14Blueprint debs
 dpkg-deb -bZgzip projects/JoyToTheWorld debs
+dpkg-deb -bZgzip projects/RedNeon debs
+dpkg-deb -bZgzip projects/Festival debs
